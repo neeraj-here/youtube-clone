e1932794c8bd5ca433d802955c9cf37195d55739
Neeraj Sharma <19bcs1298@gmail.com>
2021-05-27 16:06:03 +0530

    Style video tiles

--- src/Video.js
+++ src/Video.js
@@ -1,19 +1,32 @@
 import { Avatar } from '@material-ui/core'
 import React from 'react'
 import './Video.css'
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import ListIcon from '@material-ui/icons/List';
 
-function Video({thumbnail, title, channel, timestamp, views, channelImg}) {
+function Video({thumbnailImg, title, channelName, timestamp, views, channelImg}) {
 
     return (
         <div className="video">
-            <img className="video__thumbnail" src={thumbnail} alt="thumbnail" />
+            <div className="video__thumbnail">
+                <img className="video__thumbnailImg" src={thumbnailImg} alt="thumbnail" />
+                <div className="video__wactchOptions">
+                    <WatchLaterIcon className="video__wactchOptionsIcon" />
+                    <ListIcon className="video__wactchOptionsIcon" />
+                </div>
+                <div className="video__playButton"> <PlayArrowIcon className="video__playButtonIcon" /> </div>
+                <div className="video__duration">5:30</div>
+            </div>
             <div className="video__info">
-                <Avatar className="video__avatar" src={channelImg} alt={channel} />
+                <Avatar className="video__avatar" src={channelImg} alt={channelName} />
                 <div className="video__text">
                     <h4>{title}</h4>
-                    <p>{channel}</p>
-                    <p>{views} views <span className="video__dot"></span> {timestamp} ago</p>
+                    <p className="video__channelName" title={channelName}>{channelName}</p>
+                    <p>{views} views • {timestamp} ago</p>
                 </div>
+                <div className="video__threeDots"> <MoreVertIcon /> </div>
             </div>
         </div>
     )
